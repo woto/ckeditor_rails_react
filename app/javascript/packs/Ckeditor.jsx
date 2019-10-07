fcf8f753c794b503128e6c23b521998b968f5782
Ruslan Kornev <oganer@gmail.com>
2019-10-07 20:38:01 +0300

    It works and this is strange :)

--- app/javascript/packs/Ckeditor.jsx
+++ app/javascript/packs/Ckeditor.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import '@ckeditor/ckeditor5-build-classic';
 
 class App extends Component {
     render() {
